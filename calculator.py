1f64bff0fb44df086510fa7f3d5fac98c017e012
Ubuntu <ubuntu@ip-172-31-30-224.ec2.internal>
2019-09-26 17:14:24 +0000

    calculator that does addition

---
+++ calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin//env python3
+
+# Created on: September 2019
+# Created by: Paul Madut
+# Plagarized by: Teddy-Michael Sannan
+# This is the a program used to calculate addition
+
+
+def main():
+    # This functions calculates addition
+    # Input
+    first_integer = int(input("Enter first integer: "))
+    second_integer = int(input("Enter second integer: "))
+    # Process
+    total_value = first_integer + second_integer
+    # Output
+    print("")
+    print(first_integer, "+", second_integer, "=", total_value)
+
+
+if __name__ == "__main__":
+    main()
